dfe386660ade3a4df1007caebd9f143fdedf8765
Jiri Zizkovsky <jiri.zizkovsky@wandera.com>
2021-04-01 08:59:47 +0200

    WND-35293 Report all events

--- pkg/kube/watcher.go
+++ pkg/kube/watcher.go
@@ -7,7 +7,6 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
-	"time"
 )
 
 type EventHandler func(event *EnhancedEvent)
@@ -49,12 +48,6 @@ func (e *EventWatcher) OnUpdate(oldObj, newObj interface{}) {
 }
 
 func (e *EventWatcher) onEvent(event *corev1.Event) {
-	// TODO: Re-enable this after development
-	// It's probably an old event we are catching, it's not the best way but anyways
-	if time.Now().Sub(event.CreationTimestamp.Time) > time.Second*5 {
-		return
-	}
-
 	log.Debug().
 		Str("msg", event.Message).
 		Str("namespace", event.Namespace).
